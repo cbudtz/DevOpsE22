22c1c9e6fc7e580bb65db55add02b2c89c8e611e
Christian <chrbudtz@gmail.com>
2022-08-08 19:34:50 +0200

    John DIllerman

--- web/src/App.js
+++ web/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn <b>John Dillerman!</b>
+          Learn React and John Dillermand!
         </a>
       </header>
     </div>
