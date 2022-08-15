c9b57804d11a5bb6168daec4aa8346fd3f253199
Christian <chrbudtz@gmail.com>
2022-08-15 11:48:55 +0200

    Sane text and test

--- web/src/App.test.js
+++ web/src/App.test.js
@@ -7,6 +7,8 @@ test('renders learn react link and login button', () => {
   expect(linkElement).toBeInTheDocument();
   let button = screen.getByRole("button");
   expect(button).toHaveTextContent("Login");
+  let usernameField = screen.getByLabelText("username");
+  expect(usernameField).toHaveAttribute("placeholder","indtast brugernavn")
 });
 test('has login button',()=>{
   render(<App />);
